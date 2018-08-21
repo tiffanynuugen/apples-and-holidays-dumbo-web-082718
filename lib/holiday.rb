2c090195f1ad5c3a56f39dc39d26e2330e51eda9
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-21 04:01:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,18 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holiday_with_supplies|
+    puts "#{season.capitalize}:"
+    holiday_with_supplies.each do |holiday, supplies|
+      holiday_name = holiday.to_s.split("_")
+      holiday_name.each do |word|
+        word.capitalize!
+      end
+      new_holiday = holiday_name.join(" ")
+      puts "  #{new_holiday}: #{supplies.join(", ")}"
+    end
+  end
+end
 
 end
 
